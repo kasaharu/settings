4a12d943fa54399b01f4e8d8b3ef5d62b8be04bf
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-07 20:53:30 +0900

    [config.fish] Update fish config

--- config.fish
+++ config.fish
@@ -1,5 +1,18 @@
-alias ps='ps aux'
-
-source ~/.asdf/asdf.fish
 set -x LANG ja_JP.UTF-8
 ssh-add -K ~/.ssh/id_rsa
+
+# alias 設定
+alias ps='ps aux'
+
+## exa: https://the.exa.website/docs/command-line-options
+alias ls='exa'
+alias ll='exa -lh'
+alias la='exa -lah'
+
+# 言語設定 ----------------------------------------------------------------------
+## asdf 設定
+source ~/.asdf/asdf.fish
+
+## Starship 設定
+starship init fish | source
+
